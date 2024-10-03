7a87bcc3cad0de9abe0e4d1c84c16b8be7ac30dd
Jwell2014 <91597225+Jwell2014@users.noreply.github.com>
2024-10-03 10:56:44 +0200

    app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,21 @@ import "primereact/resources/themes/soho-dark/theme.css"
 import "/node_modules/primeflex/primeflex.css"
 import logo from './logo.svg';
 import './App.css';
+import Header from './composents/Header';
+import CardList from './composents/CardList';
 
 function App() {
+  const cardData = [
+    { title: 'Compétences', content: 'Développement Web, JavaScript, React, Node.js' },
+    { title: 'Expérience', content: '4 ans en développement, gestion de projets' },
+    { title: 'Formation', content: 'Master en informatique, diverses certifications' }
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          test
-        </a>
-      </header>
+    <div>
+      <Header />
+      <div className="container mx-auto p-4">
+        <CardList items={cardData} />
+      </div>
     </div>
   );
 }
