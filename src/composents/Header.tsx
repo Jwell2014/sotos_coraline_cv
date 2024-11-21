29063d2395e497d54cb078ccd692897cfc489b35
Jwell2014 <91597225+Jwell2014@users.noreply.github.com>
2024-11-21 17:39:52 +0100

    titre

--- src/composents/Header.tsx
+++ src/composents/Header.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Header: React.FC = () => {
     return (
         <header className="flex justify-center items-center h-40 bg-black">
-            <h1 className="text-5xl font-bold text-white">Développeur </h1>
+            <h1 className="text-5xl font-bold text-white">CONCEPTEUR </h1>
             <h1 className="text-5xl font-bold text-white">full</h1>
             <h1 className="text-5xl font-bold text-white">stack</h1>
 
