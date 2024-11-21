3911697d134f34f223ef2860814f63cb08ef5a7e
Jwell2014 <91597225+Jwell2014@users.noreply.github.com>
2024-11-21 19:46:53 +0100

    image et texte

--- src/composents/Header.tsx
+++ src/composents/Header.tsx
@@ -3,9 +3,9 @@ import React from 'react';
 const Header: React.FC = () => {
     return (
         <header className="h-40 bg-black">
-            <h1 className="w-80 text-5xl font-bold text-white">CONCEPTEUR DÉVELOPPEUR</h1>
-            <h1 className="text-5xl font-bold text-white">D'APPLICATIONS</h1>
-            <h1 className="text-5xl font-bold text-white">EN ENVIRONNEMENT DevOps</h1>
+            <h1 className="mb-0 text-7xl font-bold text-white">CONCEPTEUR DÉVELOPPEUR</h1>
+            <h1 className="my-0 text-7xl font-bold text-white">D'APPLICATIONS</h1>
+            <h1 className="my-0 text-6xl font-bold text-white">EN ENVIRONNEMENT DEVOPS</h1>
 
         </header>
     );
