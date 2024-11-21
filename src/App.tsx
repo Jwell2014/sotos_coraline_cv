c0cce6ae9f8afa0b4d90a17d37d4173aec031cf2
Jwell2014 <91597225+Jwell2014@users.noreply.github.com>
2024-11-21 20:07:53 +0100

    essaie

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { cardData } from './data/card';
 import 'primereact/resources/themes/soho-dark/theme.css';  // Thème de PrimeReact
 import 'primereact/resources/primereact.min.css';  // Styles principaux de PrimeReact
 import 'primeflex/primeflex.css';  // PrimeFlex
-import imgProfil from './assets/profilCv.svg';
+import imgProfil from './assets/coraline_mini.jpg';
 
 const App: React.FC = () => {
   return (
