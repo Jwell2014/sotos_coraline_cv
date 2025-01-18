00f3bbd9b64cd75a62fc6ccf5a2ef2d0a3c01168
Jwell2014 <91597225+Jwell2014@users.noreply.github.com>
2025-01-18 14:39:25 +0100

    ajout bouton réseau

--- src/App.tsx
+++ src/App.tsx
@@ -11,13 +11,15 @@ import 'primeflex/primeflex.css';
 import 'primeicons/primeicons.css';
 import TimelineDiplome from './composents/TimelineDiplome';
 import { Button } from 'primereact/button';
+import SelectedButton from './composents/Button';
+import { Dialog } from 'primereact/dialog';
 
 const App: React.FC = () => {
   const [isOpen, setIsOpen] = useState(Boolean);
   const [items, setItems] = useState([{}]);
   const [onClickValue, setOnClickValue] = useState("");
-  console.log("ok" + onClickValue);
-  console.log("ok" + isOpen);
+  const [visible, setVisible] = useState<boolean>(false);
+
 
 
 
@@ -50,12 +52,29 @@ const App: React.FC = () => {
       </div>
       <div className={isOpen ? "" : "hidden"} >
         <div className="flex justify-content-end  mb-3 px-4 py-2">
-          <Button icon="pi pi-times" rounded outlined severity="danger" aria-label="Cancel" onClick={handleClose} />
+          <Button icon="pi pi-times" rounded outlined severity="info" aria-label="Cancel" onClick={handleClose} />
         </div>
         <TimelineDiplome items={items} />
       </div>
-      <div>
+      <div className='px-4 py-6'>
         <CardList items={cardData} />
+      </div>
+      <div className='flex justify-content-evenly'>
+        <SelectedButton icon="pi pi-linkedin" classElem="mt-5" label="LinKedin" onClick={() => window.open('https://www.linkedin.com/in/coraline-sotos-72bb7512a/', '_blank')} />
+        <SelectedButton icon="pi pi-github" classElem="mt-5" label="GitHub" onClick={() => window.open('https://github.com/Jwell2014', '_blank')} />
+        <SelectedButton icon="pi pi-folder" classElem="mt-5" label="PorteFolio" onClick={() => setVisible(true)} />
+
+      </div>
+      <div className="card flex justify-content-center">
+        <Dialog header="✨ Mon Portfolio est en Construction ! ✨" visible={visible} style={{ width: '60vw' }} onHide={() => { if (!visible) return; setVisible(false); }}>
+          <p className="m-0">
+            Bonjour et merci pour votre visite !<br /><br />
+            Je travaille actuellement à la création d’un portfolio qui mettra en lumière mes compétences, mes projets et mes réalisations.<br />
+            Ce sera un espace unique où vous pourrez découvrir mon travail sous son meilleur jour. 🌟<br /><br />
+            Le portfolio sera bientôt prêt, alors restez à l’écoute ! En attendant, n’hésitez pas à me contacter si vous souhaitez en savoir plus sur mes projets ou collaborer avec moi. 🙌<br /><br />
+            Merci pour votre patience et votre compréhension ! 😊
+          </p>
+        </Dialog>
       </div>
     </div>
   );
